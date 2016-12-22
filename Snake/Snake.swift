459058288e04fb2b774f1aa4bc58394dda866d0b
Marcus Quettan <mquettan@Marcuss-MBP.hsd1.ga.comcast.net>
2016-12-22 08:09:36 -0500

    Starting Snake Logic

--- Snake/Snake.swift
+++ Snake/Snake.swift
@@ -7,7 +7,47 @@
 //
 
 import Foundation
+import SpriteKit
+
+enum Direction {
+    case Left
+    case Right
+    case Down
+    case Up
+}
 
 class Snake {
+    var points = [CGPoint]()
+    var width: CGFloat = 30
+    
+    init() {
+        
+    }
+    
+    init(screenWidth: Int, screenHeight: Int, gridWidth: Int, gridHeight: Int){
+        
+    }
+    
+    func move(direction: Direction) {
+        switch direction {
+        case .Left:
+            //New Head position is one width left of the current position.
+            points.insert(CGPoint(x: (self.points[0].x - self.width), y: points[0].y), at: 0)
+            points.removeLast()
+        case .Right:
+            //New Head position is one width right of the current position.
+            points.insert(CGPoint(x: (points[0].x + self.width), y: points[0].y), at: 0)
+            points.removeLast()
+        case .Up:
+            //New Head position is one width above of the current position.
+            points.insert(CGPoint(x: points[0].x, y: (points[0].y - self.width)), at: 0)
+            points.removeLast()
+        case .Down:
+            //New Head position is one width below of the current position.
+            points.insert(CGPoint(x: points[0].x, y: (points[0].y + self.width)), at: 0)
+            points.removeLast()
+        }
+    }
+    
     
 }
