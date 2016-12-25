b40e7466d16e4afc5a0eb0461a51cfbd144ab9a8
Marcus Quettan <mquettan@Marcuss-MacBook-Pro.local>
2016-12-25 16:36:37 -0500

    Completed Edges.  Working on food generation to fill the whole screen.

--- Snake/GameScene.swift
+++ Snake/GameScene.swift
@@ -12,15 +12,26 @@ import GameplayKit
 class GameScene: SKScene {
     
     private var label : SKLabelNode?
-    private var snakeLogic = SnakeLogic()
+    private var snakeLogic: SnakeLogic?
     private var snakeSegments: [SKShapeNode] = [SKShapeNode()]
     private var snakeSize: CGSize!
+    private var food = SKSpriteNode(imageNamed: "apple")
+    private var timer : Timer!
+    private var snakeScale = 30
     
     override func didMove(to view: SKView) {
+        //Init Snake Logic Class
+        snakeLogic = SnakeLogic(worldSize: self.frame.size, snakeSize: CGSize(width: snakeScale, height: snakeScale))
+        
         //Create the Snake's Head
-        snakeSize = CGSize(width: self.frame.width/30, height: self.frame.width/30)
+        snakeSize = CGSize(width: snakeLogic!.snakeSize.width, height: snakeLogic!.snakeSize.height)
         snakeSegments[0] = SKShapeNode(rectOf: snakeSize)
         self.addChild(snakeSegments[0])
+        
+        //Create initial food
+        self.food.position = snakeLogic!.foodLocation
+        self.food.size = snakeSize
+        self.addChild(food)
         
         //Start the four gesture recognizers for each swipe direction
         //Up
@@ -40,6 +51,9 @@ class GameScene: SKScene {
         swipeRight.direction = UISwipeGestureRecognizerDirection.right
         self.view!.addGestureRecognizer(swipeRight)
         
+        //Start Game Loop Timer
+        //self.timer = timer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "timerMethod:", userInfo: nil, repeats: true)
+        self.timer = Timer.scheduledTimer(timeInterval: Difficulty.Easy.rawValue, target: self, selector: #selector(self.timerMethod), userInfo: nil, repeats: true)
     }
     
     @objc private func respondToSwipeGesture(gesture: UIGestureRecognizer) {
@@ -47,43 +61,47 @@ class GameScene: SKScene {
         if let swipeGesture = gesture as? UISwipeGestureRecognizer {
             switch swipeGesture.direction {
             case UISwipeGestureRecognizerDirection.right:
-                snakeLogic.currentDirection = .Right
+                snakeLogic!.currentDirection = .Right
             case UISwipeGestureRecognizerDirection.down:
-                snakeLogic.currentDirection = .Down
+                snakeLogic!.currentDirection = .Down
             case UISwipeGestureRecognizerDirection.left:
-                snakeLogic.currentDirection = .Left
+                snakeLogic!.currentDirection = .Left
             case UISwipeGestureRecognizerDirection.up:
-                snakeLogic.currentDirection = .Up
+                snakeLogic!.currentDirection = .Up
             default:
                 break
             }
         }
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
+    @objc private func timerMethod() {
         //Move Every Update
-        //Fix this to be variable on game difficulty setting
-        //snakeLogic.eat()
-        //snakeSegments.append(SKShapeNode(rectOf: snakeSize))
-        snakeLogic.move()
-        if((abs(snakeLogic.points[0].x) * 2) > self.frame.width) {
-            snakeLogic.points[0].x *= -1
-        }
-        if((abs(snakeLogic.points[0].y) * 2) > self.frame.height) {
-            snakeLogic.points[0].y *= -1
-        }
+        snakeLogic!.move()
         
+        //Handle the case where we're eating food for this update
+        if(snakeLogic!.didEatFood) {
+            snakeLogic!.generateFood()
+            food.position = snakeLogic!.foodLocation
+            //food.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            print("Created food at \(food.position)")
+            snakeSegments.append(SKShapeNode(rectOf: snakeSize))
+            snakeLogic!.didEatFood = false
+        }
         
         //Update the snake drawing for each point in snake
         var i: Int = 0
         for segment in snakeSegments {
-            segment.position = snakeLogic.points[i]
+            segment.position = snakeLogic!.points[i]
             i += 1
             segment.removeFromParent()
             self.addChild(segment)
         }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        //Everything is done in the timerMethod
         
     }
 }
--- Snake/SnakeLogic.swift
+++ Snake/SnakeLogic.swift
@@ -16,33 +16,52 @@ enum Direction {
     case Up
 }
 
+enum Difficulty: Double {
+    case Easy = 0.75
+    case Medium = 0.5
+    case Hard = 0.25
+}
+
 class SnakeLogic {
-    var points: [CGPoint] = [CGPoint(x: 0, y: 0)]
-    var width: CGFloat = 30
-    var currentDirection : Direction = .Up
-    private var didEatFood = false
-    var foodLocation = CGPoint(x: 0, y: 0)
+    var points: [CGPoint]
+    var snakeSize: CGSize
+    var currentDirection: Direction
+    var didEatFood: Bool
+    var foodLocation: CGPoint
+    private var worldSize: CGSize
+    
+    init(worldSize: CGSize, snakeSize: CGSize) {
+        self.worldSize = worldSize
+        self.snakeSize = snakeSize
+        self.foodLocation = CGPoint(x: 0, y: 0)
+        self.didEatFood = false
+        self.points = [CGPoint(x: 0, y: 0)]
+        self.currentDirection = .Up
+    }
     
     func move(_ direction: Direction) {
         switch direction {
         case .Left:
             //New Head position is one width left of the current position.
-            self.points.insert(CGPoint(x: (self.points[0].x - self.width), y: self.points[0].y), at: 0)
+            self.points.insert(CGPoint(x: (self.points[0].x - self.snakeSize.width), y: self.points[0].y), at: 0)
         case .Right:
             //New Head position is one width right of the current position.
-            self.points.insert(CGPoint(x: (self.points[0].x + self.width), y: self.points[0].y), at: 0)
+            self.points.insert(CGPoint(x: (self.points[0].x + self.snakeSize.width), y: self.points[0].y), at: 0)
         case .Up:
             //New Head position is one width above of the current position.
-            self.points.insert(CGPoint(x: self.points[0].x, y: (self.points[0].y + self.width)), at: 0)
+            self.points.insert(CGPoint(x: self.points[0].x, y: (self.points[0].y + self.snakeSize.height)), at: 0)
         case .Down:
             //New Head position is one width below of the current position.
-            self.points.insert(CGPoint(x: self.points[0].x, y: (self.points[0].y - self.width)), at: 0)
+            self.points.insert(CGPoint(x: self.points[0].x, y: (self.points[0].y - self.snakeSize.height)), at: 0)
         }
         
-        if(didEatFood) {
-            //If we've eaten, when we move don't remove the last point this time.
-            self.didEatFood = false
-        } else {
+        self.wrapEdges()
+        
+        if(self.points[0] == self.foodLocation){
+            self.didEatFood = true
+        }
+        
+        if(!self.didEatFood) {
             //If we haven't eaten, remove the last point so the snake moves
             self.points.removeLast()
         }
@@ -52,13 +71,30 @@ class SnakeLogic {
         self.move(currentDirection)
     }
     
+    func wrapEdges() {
+        //If we've hit the edge of the world, loop to the other side!
+        if(((abs(self.points[0].x) + (self.snakeSize.width / 2)) * 2) > self.worldSize.width) {
+            if(self.points[0].x < 0) {
+                self.points[0].x += self.snakeSize.width
+            } else {
+                self.points[0].x -= self.snakeSize.width
+            }
+            self.points[0].x *= -1
+            
+        }
+        if(((abs(self.points[0].y) + (snakeSize.height / 2)) * 2) > self.worldSize.height) {
+            if(self.points[0].y < 0) {
+                self.points[0].y += self.snakeSize.height
+            } else {
+                self.points[0].y -= self.snakeSize.height
+            }
+            self.points[0].y *= -1
+        }
+    }
+    
     func generateFood() {
-        
+        self.foodLocation.x = CGFloat(UInt32(self.snakeSize.width) * arc4random_uniform(6))
+        self.foodLocation.y = CGFloat(UInt32(self.snakeSize.height) * arc4random_uniform(6))
     }
-    
-    func eat() {
-        self.didEatFood = true;
-    }
-    
     
 }
