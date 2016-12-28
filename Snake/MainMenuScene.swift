33d78728b91f40eccab73d5bcacd57bd062d11f1
Marcus Quettan <mquettan@Marcuss-MBP.hsd1.ga.comcast.net>
2016-12-28 00:18:26 -0500

    Working Start Game button.

---
+++ Snake/MainMenuScene.swift
@@ -0,0 +1,33 @@
+//
+//  MainMenuScene.swift
+//  Snake
+//
+//  Created by Marcus Quettan on 12/27/16.
+//  Copyright © 2016 Marcus Quettan. All rights reserved.
+//
+import SpriteKit
+import GameplayKit
+
+class MainMenuScene: SKScene {
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("Touch Happened")
+        if let location = touches.first?.location(in: self) {
+            let nodeArray = self.nodes(at: location)
+            for node in nodeArray {
+                //If Start Button is Pressed
+                if (node.name == "StartButton") {
+                    let startGameTransition = SKTransition.fade(withDuration: 0.5)
+                    self.view?.presentScene(SKScene(fileNamed: "GameScene")!, transition: startGameTransition);
+                }
+                
+                //If Leaderboards Button is Pressed
+                if (node.name == "LeaderboardButton") {
+                    let startGameTransition = SKTransition.fade(withDuration: 0.5)
+                    self.view?.presentScene(SKScene(fileNamed: "GameScene")!, transition: startGameTransition);
+                }
+                
+            }
+        }
+    }
+}
